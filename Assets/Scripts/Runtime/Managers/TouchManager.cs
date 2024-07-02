d3fd2f17082a5f056f307ddc1df0b78a5c9ffcbc
Alparslan Yurtseven <86921625+AlpArslan07TR@users.noreply.github.com>
2024-07-02 21:49:12 +0300

    Update TouchManager

--- Assets/Scripts/Runtime/Managers/TouchManager.cs
+++ Assets/Scripts/Runtime/Managers/TouchManager.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Camera cam;
+    [SerializeField] string collisionTag;
+    bool _canTouch;
+
     void Start()
     {
-        
+        _canTouch = false;
+        StartCoroutine(WaitForTouch_Cor());
+    }
+    
+    IEnumerator WaitForTouch_Cor()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        _canTouch = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_canTouch)
+        {
+            GetTouch(Input.mousePosition);
+        }
     }
+    
+    private void GetTouch(Vector3 pos)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.OverlapPoint(cam.ScreenToWorldPoint(pos));
+            if (CanTouch(hit))
+            {
+                //var selectedCard = hit.gameObject.GetComponent<Card>();
+                //TouchEvents.OnCardTapped?.Invoke(selectedCard);
+            }
+            else
+            {
+                //TouchEvents.OnEmptyTapped?.Invoke();
+            }
+        }
+    }
+
+    private bool CanTouch(Collider2D hit)
+    {
+        return hit != null && hit.CompareTag(collisionTag);
+    }
+
+   
 }
