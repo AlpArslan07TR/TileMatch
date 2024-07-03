2170c423a7010aeac4bb0cd06232e6d05ed63f4c
Alparslan Yurtseven <86921625+AlpArslan07TR@users.noreply.github.com>
2024-07-03 21:07:13 +0300

    Tile Data Created

---
+++ Assets/Scripts/Runtime/GamePlay/Levels/LevelData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct LevelData 
+{
+    public string title;
+    public Tile[] tile;
+}
