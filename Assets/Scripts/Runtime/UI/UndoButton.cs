9cb1458edc24211d1990b6c85e7d15b402b525f6
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2024-07-23 22:23:35 +0300

    Undo Button

---
+++ Assets/Scripts/Runtime/UI/UndoButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UndoButton : MonoBehaviour
+{
+    [SerializeField] Button button;
+    [SerializeField] Board board;
+
+
+    
+}
